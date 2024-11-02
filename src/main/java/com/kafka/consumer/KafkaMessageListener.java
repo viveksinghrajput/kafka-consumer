b3c2842b57580752f9a349f860fdf278e645f02d
vivek singh rajput <viveksingh6262@rediffmail.com>
2024-11-02 09:03:29 +0530

    consume message from a particular partition from a topic

--- src/main/java/com/kafka/consumer/KafkaMessageListener.java
+++ src/main/java/com/kafka/consumer/KafkaMessageListener.java
@@ -5,6 +5,7 @@ import com.kafka.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 
@@ -13,9 +14,16 @@ public class KafkaMessageListener {
 
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-    @KafkaListener(topics = "kafka-demo",groupId = "jt-group")
+    @KafkaListener(topics = "kafka-demo", groupId = "kafka-consumer"
+            , topicPartitions = {@TopicPartition(topic = "kafka-demo", partitions = {"3"})})
     public void consumeEvents(Customer customer) {
         log.info("consumer consume the events {} ", customer.toString());
+    }
+
+    @KafkaListener(topics = "kafka-demo", groupId = "kafka-consumer",
+            topicPartitions = {@TopicPartition(topic = "kafka-demo", partitions = {"3"})})
+    public void consume(String message) {
+        log.info("consumer2 consume the message " + message);
     }
 
    /* @KafkaListener(topics = "kafka-demo",groupId = "kafka-consumer")
